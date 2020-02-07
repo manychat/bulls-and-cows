88c30c2ae0105580e762078025809e9960a4be0e
Evgenii <zer0.stat@mail.ru>
2020-02-08 00:15:11 +0300

    [slim] Add Slim

--- public/index.php
+++ public/index.php
@@ -2,6 +2,23 @@
 
 declare(strict_types=1);
 
-echo json_encode([
-    'Hello world',
-]);
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// run application
+(function (): void {
+    $app = AppFactory::create();
+    $app->get(
+        '/',
+        function (Request $request, Response $response, $args) {
+            $response->getBody()->write('Hello world!');
+
+            return $response;
+        }
+    );
+
+    $app->run();
+})();
