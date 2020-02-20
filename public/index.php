8c9606766b2c34b388b8de7da86a2f81680c149e
Evgenii <zer0.stat@mail.ru>
2020-02-20 19:03:37 +0300

    [fix] Class name case

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-use Src\Infrastructure\Framework\AppBuilder\APIDirector;
+use Src\Infrastructure\Framework\AppBuilder\ApiDirector;
 
 !defined('ROOT_DIR') && define('ROOT_DIR', dirname(__DIR__));
 
 require ROOT_DIR . '/vendor/autoload.php';
 
-APIDirector::build()->run();
+ApiDirector::build()->run();
