2249ad1d4ab8e4d34f88dfe23986345c4aa4ae1c
Evgenii <zer0.stat@mail.ru>
2020-03-09 18:38:08 +0300

    [test] Score test

---
+++ tests/Unit/Model/Game/Score/ScoreTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Model\Game\Score;
+
+use PHPUnit\Framework\TestCase;
+use Src\Bc\Domain\Model\Game\Score\Score;
+
+final class ScoreTest extends TestCase
+{
+    private const TEST_SUBSCRIBER_ID = 1;
+
+    public function testIsBelongsToShouldReturnTrue(): void
+    {
+        self::assertTrue($this->getScore(self::TEST_SUBSCRIBER_ID)->isBelongsTo(self::TEST_SUBSCRIBER_ID));
+    }
+
+    public function testIsBelongsToShouldReturnFalse(): void
+    {
+        $score = $this->getScore(self::TEST_SUBSCRIBER_ID);
+        $anotherSubscriberId = 2;
+
+        self::assertFalse($score->isBelongsTo($anotherSubscriberId));
+    }
+
+    private function getScore(int $subscriberId): Score
+    {
+        return new Score($subscriberId, '', 0);
+    }
+}
