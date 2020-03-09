03a59c2638bf2dd7a9735768e6e0e56ae0e052fc
Evgenii <zer0.stat@mail.ru>
2020-03-09 18:27:24 +0300

    [test] Level test

--- tests/Unit/Model/Game/FiguresTest.php
+++ tests/Unit/Model/Game/FiguresTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Test\Unit\Model\Game;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Src\Bc\Domain\Model\Game\Figures;
 
@@ -11,21 +12,21 @@ final class FiguresTest extends TestCase
 {
     public function testConstructShouldFailWhenFiguresAreNotNumbers(): void
     {
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
 
         new Figures('not-a-number');
     }
 
     public function testConstructShouldFailWhenFiguresAreNotUnique(): void
     {
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
 
         new Figures('0122');
     }
 
     public function testConstructShouldFailWhenNotFourFigures(): void
     {
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
 
         new Figures('01234');
     }
---
+++ tests/Unit/Model/Game/LevelTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Model\Game;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Src\Bc\Domain\Model\Game\Level;
+
+final class LevelTest extends TestCase
+{
+    public function testConstructShouldFailWhenLevelIsInvalid(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Level('invalid-level');
+    }
+
+    public function testConstructShouldSuccess(): void
+    {
+        $level = new Level(Level::EASY);
+
+        self::assertEquals(Level::EASY, $level->getLevel());
+    }
+
+    public function testToStringShouldSuccess(): void
+    {
+        $level = new Level(Level::EASY);
+
+        self::assertEquals(Level::EASY, (string)$level);
+    }
+
+    public function testIsHardShouldReturnTrueWhenItIsHard(): void
+    {
+        $level = new Level(Level::HARD);
+
+        self::assertTrue($level->isHard());
+    }
+
+    public function testIsHardShouldReturnFalseWhenItIsNotHard(): void
+    {
+        $level = new Level(Level::EASY);
+
+        self::assertFalse($level->isHard());
+    }
+}
